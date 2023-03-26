6861dbc27d37b3d5dffc6d47e8e5570d4cdd0852
mscotti <mscotti@pigmalion.com.ar>
2023-03-26 00:13:18 -0300

    CLIENT - Auth redirect, styles.

--- client/pages/api/auth/[...auth0].js
+++ client/pages/api/auth/[...auth0].js
@@ -1,3 +1,14 @@
-import { handleAuth } from '@auth0/nextjs-auth0';
+import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-export default handleAuth();
+export default handleAuth({
+  async login(req, res) {
+    try {
+      await handleLogin(req, res, {
+        returnTo: req.headers.referer || '/',
+      });
+    } catch (error) {
+      res.status(error.status || 400).end(error.message);
+    }
+  },
+});
+
