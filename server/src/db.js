470cd10aee9298b897b7c470847b4c1e748b03f3
mscotti <mscotti@pigmalion.com.ar>
2023-08-04 11:43:53 -0300

    Se dockerizó la api y se agregaron fotos para el readme

--- server/src/db.js
+++ server/src/db.js
@@ -1,51 +1,57 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT, DB_URL} = process.env;
 const RoomModel = require('./models/Room');
 const StudentModel = require('./models/Student');
 
-const sequelize = process.env.NODE_ENV === 'production' ?
-    new Sequelize({
-        database: DB_NAME,
-        dialect: 'postgres',
-        host: DB_HOST,
-        port: DB_PORT,
-        username: DB_USER,
-        password: DB_PASSWORD,
-        pool: {
-            max: 3,
-            min: 1,
-            idle: 10000,
-        },
-        dialectOptions: {
-            ssl: {
-                require: true,
-                rejectUnauthorized: false,
-            },
-            keepAlive: true,
-        },
-        ssl: true,
-    }) : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-            host: 'localhost',
-            dialect: 'postgres',
-            logging: false,
-        });
+// const sequelize = process.env.NODE_ENV === 'production' ?
+//     new Sequelize({
+//         database: DB_NAME,
+//         dialect: 'postgres',
+//         host: DB_HOST,
+//         port: DB_PORT,
+//         username: DB_USER,
+//         password: DB_PASSWORD,
+//         pool: {
+//             max: 3,
+//             min: 1,
+//             idle: 10000,
+//         },
+//         dialectOptions: {
+//             ssl: {
+//                 require: true,
+//                 rejectUnauthorized: false,
+//             },
+//             keepAlive: true,
+//         },
+//         ssl: true,
+//     }) : new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+//             host: 'localhost',
+//             dialect: 'postgres',
+//             logging: false,
+//         });
 
-const Room = RoomModel(sequelize);
+const sequelize = new Sequelize(DB_URL)
+
 const Student = StudentModel(sequelize);
-Room.hasMany(Student, { foreignKey: 'RoomId'});
-Student.belongsTo(Room);
-Student.belongsToMany
-    (
-        Student, 
-        {
-            through: 'Sibling',
-            as: 'siblings',
-            foreignKey: 'studentId',
-            otherKey: 'siblingId',
-            timestamps: false
-        }
-    )
+const Room = RoomModel(sequelize);
+
+// Step 1
+// Student.belongsToMany
+//     (
+//         Student, 
+//         {
+//             through: 'Sibling',
+//             as: 'siblings',
+//             foreignKey: 'studentId',
+//             otherKey: 'siblingId',
+//             timestamps: false
+//         }
+//     )
+
+// Step 2
+// Student.belongsTo(Room);
+// Room.hasMany(Student, { foreignKey: 'RoomId'});
 
 sequelize.sync();
 
